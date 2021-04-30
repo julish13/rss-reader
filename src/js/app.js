d8d9c02c15ce483d001f94fc01a0fd6de6af0d0f
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-04-30 18:31:26 +0300

    feat (app.js): set controller

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,39 @@
-import onChange from 'on-change';
-import validateUrl from './validation.js';
+import _ from 'lodash';
+import axios from 'axios';
+import parseFeed from './parser.js';
+import validateUrl from './validator.js';
+import watchedState from './view.js';
 
 const form = document.querySelector('.rss-form');
-const feeds = document.querySelector('.feeds');
-const posts = document.querySelector('.posts');
 
 export default () => {
-  const state = {
-    form: {
-      validationError: null,
-      processError: null,
-    },
-    feeds: [],
-    posts: [],
-  };
-
-  const watchedState = onChange(state, (path, value) => {
-
-  });
-
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const data = formData.get('url');
-    validateUrl(data)
-      .then((url) => {
-        watchedState.feeds.push(url);
-        watchedState.form.validationError = null;
+    const url = formData.get('url');
+    validateUrl(url, watchedState.data.feeds)
+      .then((link) => {
+        watchedState.form.error = null;
+        return link;
       })
-      .catch(({ errors: [error] }) => { watchedState.form.validationError = error; });
-    // не уверена что это ок, но предполагается, что ошибка 1 всегда
-    console.log(state);
+      .then((link) => {
+        const urlProxified = `https://hexlet-allorigins.herokuapp.com/get?url=${
+          encodeURIComponent(link)
+        }`;
+        return axios.get(urlProxified);
+      })
+      .then(({ data: { contents } }) => {
+        const id = _.uniqueId();
+        const { title, description, posts } = parseFeed(contents);
+        watchedState.data.feeds.push({
+          id, url, title, description,
+        });
+        watchedState.data.posts[id] = posts;
+        watchedState.form.processState = 'succeed';
+      })
+      .catch((error) => {
+        watchedState.form.error = error;
+        watchedState.form.processState = 'invalid';
+      });
   });
 };
